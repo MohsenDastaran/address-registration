fec2c53806ba7724acedd9cce116beaee4b82ea3
MohsenDastaran <mohsen.dastaran@gmail.com>
2024-12-12 22:02:57 +0330

    feat: add create address page

--- app/components/Ui/Input.vue
+++ app/components/Ui/Input.vue
@@ -1,6 +1,40 @@
 <template>
-  <!-- eslint-disable-next-line vue/html-self-closing -->
-  <input v-bind="props" v-model="localModel" :class="styles({ class: props.class })" />
+  <div class="relative">
+    <!-- eslint-disable-next-line vue/html-self-closing -->
+    <input v-bind="props" v-model="localModel" :class="styles({ class: props.class })" />
+    <svg
+      class="clear absolute hidden cursor-pointer"
+      v-if="localModel"
+      @click="localModel = ''"
+      style="top: 12px; left: 15px"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_6073_721)">
+        <circle cx="8" cy="8" r="8" fill="#B6B6B6" />
+        <path
+          d="M4.5354 11.2991L11.6065 4.43296"
+          stroke="white"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M11.6065 11.2992L4.5354 4.43311"
+          stroke="white"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_6073_721">
+          <rect width="16" height="16" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -21,8 +55,16 @@
     "update:modelValue": [value: any];
   }>();
   const localModel = useVModel(props, "modelValue", emits);
-
   const styles = tv({
-    base: "form-input h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:px-1 file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground file:hover:cursor-pointer focus:border-input focus:ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:[color-scheme:dark] sm:text-sm",
+    base: "form-input h-10 rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:px-1 file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground file:hover:cursor-pointer focus:border-input focus:ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:[color-scheme:dark] sm:text-sm",
   });
 </script>
+<style scoped>
+  /* Show SVG when input is focused */
+  input:focus + .clear {
+    display: inline;
+  }
+  input {
+    width: 98% !important;
+  }
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,38 +2,13 @@
   <div>
     <UiNavbar sticky>
       <UiContainer class="flex h-14 items-center justify-between">
-        <NuxtLink class="text-lg font-semibold" to="/">{{ SITE_NAME }}</NuxtLink>
-        <!-- <div class="flex items-center gap-2">
-          <UiButton
-            external
-            href="https://github.com/MohsenDastaran/shadcn-table"
-            target="_blank"
-            variant="ghost"
-            size="icon-sm"
-            title="View on github"
-          >
-            <span class="sr-only">View on github</span>
-            <Icon name="lucide:github" class="size-4" />
-          </UiButton>
-          <UiDropdownMenu>
-            <UiDropdownMenuTrigger as-child>
-              <UiButton class="h-9 w-9" variant="ghost" size="icon" title="Theme switcher">
-                <span class="sr-only">Theme switcher</span>
-                <Icon :name="currentIcon || 'lucide:sun'" class="h-[18px] w-[18px]"
-              /></UiButton>
-            </UiDropdownMenuTrigger>
-            <UiDropdownMenuContent align="end" :side-offset="5">
-              <UiDropdownMenuItem
-                class="cursor-pointer"
-                v-for="(m, i) in modes"
-                :key="i"
-                :icon="m.icon"
-                :title="m.title"
-                @click="setTheme(m.value)"
-              />
-            </UiDropdownMenuContent>
-          </UiDropdownMenu>
-        </div> -->
+        <NuxtLink class="text-lg" to="/">
+          <img style="scale: 0.9" src="@/assets/achare.png" alt="Achareh" />
+        </NuxtLink>
+        <div>
+          <NuxtLink class="me-2" to="/">ثبت آدرس </NuxtLink>
+          <NuxtLink class="text-achareh" to="/registered">مشاهده آدرس‌ها </NuxtLink>
+        </div>
       </UiContainer>
     </UiNavbar>
     <slot />
@@ -41,18 +16,9 @@
 </template>
 
 <script lang="ts" setup>
-  const modes = [
-    { icon: "lucide:sun", title: "Light", value: "light" },
-    { icon: "lucide:moon", title: "Dark", value: "dark" },
-    { icon: "lucide:laptop", title: "System", value: "system" },
-  ];
-
   const colorMode = useColorMode();
-  const setTheme = (val: string) => {
-    colorMode.preference = val;
-  };
-
-  const currentIcon = computed(() => {
-    return modes.find((m) => m.value === colorMode?.preference)?.icon;
-  });
+  // const setTheme = (val: string) => {
+  colorMode.preference = "light";
+  // };
+  //
 </script>
